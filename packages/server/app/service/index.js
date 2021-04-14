0cb0dc3b9b2b818260763a6b545718e55b663bd2
oblivioussing <oblivioussing@outlook.com>
2021-04-14 20:35:45 +0800

    json

--- packages/server/app/service/index.js
+++ packages/server/app/service/index.js
@@ -1,9 +1,14 @@
 const Service = require('egg').Service
+const fs = require('fs')
+const path = require('path')
 let result = require('../model/result')
 
 class IndexService extends Service {
   // 保存模版
   async save(body) {
+    // const url = path.join(__dirname, 'data', 'data.json')
+    const ret = fs.writeFileSync('../data/data.json', JSON.stringify(body))
+    console.log(ret)
     return result
   }
 }
