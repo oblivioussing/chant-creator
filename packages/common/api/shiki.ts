730b37d612fedc287baa4b95f43024628adbbc9e
oblivioussing <oblivioussing@outlook.com>
2021-04-20 19:14:11 +0800

    customMessage优化

--- packages/common/api/shiki.ts
+++ packages/common/api/shiki.ts
@@ -62,7 +62,7 @@ class Shiki {
     }
   }
   // 自定义message方法
-  customMessage = (msg: string): void => {}
+  customMessage = (msg: string) => msg
 
   // get请求
   async get(url: string, config: RequestConfig): Promise<any> {
